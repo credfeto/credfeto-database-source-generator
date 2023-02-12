f0445a4e59f46ffa6737c888159ba19514cbe113
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-02-12 19:52:36 +0000

    Split into two files

---
+++ src/Credfeto.Database.Source.Generation.Tests/DatabaseCodeGeneratorEmptyTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Credfeto.Database.Source.Generation.Tests.Verifiers;
+using Xunit;
+
+namespace Credfeto.Database.Source.Generation.Tests;
+
+[SuppressMessage(category: "Meziantou.Analyzer", checkId: "MA0051:Method too long", Justification = "Test")]
+public sealed class DatabaseCodeGeneratorEmptyTests : GeneratorVerifierTestsBase<DatabaseCodeGenerator>
+{
+    [Fact]
+    public Task NothingToGenerateGeneratesNothingAsync()
+    {
+        const string test = @"
+    namespace ConsoleApplication1
+    {
+        public static class EmptyClass
+        {
+        }
+    }";
+
+        return VerifyAsync(code: test, Array.Empty<(string filename, string generated)>());
+    }
+}
--- src/Credfeto.Database.Source.Generation.Tests/DatabaseCodeGeneratorScalarFunctionTests.cs
+++ src/Credfeto.Database.Source.Generation.Tests/DatabaseCodeGeneratorScalarFunctionTests.cs
@@ -1,4 +1,3 @@
-﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Credfeto.Database.Source.Generation.Tests.Verifiers;
@@ -7,22 +6,8 @@ using Xunit;
 namespace Credfeto.Database.Source.Generation.Tests;
 
 [SuppressMessage(category: "Meziantou.Analyzer", checkId: "MA0051:Method too long", Justification = "Test")]
-public sealed class DatabaseCodeGeneratorTests : GeneratorVerifierTestsBase<DatabaseCodeGenerator>
+public sealed class DatabaseCodeGeneratorScalarFunctionTests : GeneratorVerifierTestsBase<DatabaseCodeGenerator>
 {
-    [Fact]
-    public Task NothingToGenerateGeneratesNothingAsync()
-    {
-        const string test = @"
-    namespace ConsoleApplication1
-    {
-        public static class EmptyClass
-        {
-        }
-    }";
-
-        return VerifyAsync(code: test, Array.Empty<(string filename, string generated)>());
-    }
-
     [Fact]
     public Task SimpleScalarFunctionAsync()
     {
