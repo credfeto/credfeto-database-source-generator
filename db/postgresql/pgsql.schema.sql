f499b8d94271c921641ff93c77ee37dc9865e6c0
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-03-31 17:06:40 +0100

    Stopped TSQLLINT from complaining

--- db/postgresql/pgsql.schema.sql
+++ db/postgresql/pgsql.schema.sql
@@ -1,54 +1,60 @@
-create schema if not exists ethereum;
+SET ANSI_NULLS ON;
+SET NOCOUNT ON;
+SET QUOTED_IDENTIFIER ON;
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 
-create table if not exists ethereum.account
-(
-  id      serial
-  constraint account_pk
-  primary key,
-  name    varchar(200) not null,
-  address char(42)     not null
-  );
-
-alter table ethereum.account
-  owner to markr;
-
-create or replace function account_get(id integer)
-    returns TABLE(id integer, name text, address character)
-    language sql
-as
-$$
-SELECT a.id, a.name, a.address from ethereum.account a where a.id = account_get.id;
-$$;
-
-alter function ethereum.account_get(integer) owner to markr;
-
-create or replace function ethereum.account_getall(address character)
-    returns TABLE(id integer, name text, address character)
-    language sql
-as
-$$
-SELECT a.id, a.name, a.address from ethereum.account a where a.address = account_getall.address;
-$$;
-
-alter function ethereum.account_getall(char) owner to markr;
-
-create or replace procedure ethereum.account_insert(IN name character varying, IN address character)
-    language sql
-as
-$$
-    INSERT INTO ethereum.account (name, address)
-    VALUES (account_insert.name, account_insert.address);
-$$;
-
-alter procedure ethereum.account_insert(varchar, char) owner to markr;
-
-
-create or replace function ethereum.get_meaning_of_life_universe_and_everything()
-  returns int
-  language sql
-AS
-$$
-SELECT 42;
-$$;
-
-alter procedure ethereum.get_meaning_of_life_universe_and_everything() owner to markr;
+SELECT @@VERSION;
+-- create schema if not exists ethereum;
+--
+-- create table if not exists ethereum.account
+-- (
+--   id      serial
+--   constraint account_pk
+--   primary key,
+--   name    varchar(200) not null,
+--   address char(42)     not null
+--   );
+--
+-- alter table ethereum.account
+--   owner to markr;
+--
+-- create or replace function account_get(id integer)
+--     returns TABLE(id integer, name text, address character)
+--     language sql
+-- as
+-- $$
+-- SELECT a.id, a.name, a.address from ethereum.account a where a.id = account_get.id;
+-- $$;
+--
+-- alter function ethereum.account_get(integer) owner to markr;
+--
+-- create or replace function ethereum.account_getall(address character)
+--     returns TABLE(id integer, name text, address character)
+--     language sql
+-- as
+-- $$
+-- SELECT a.id, a.name, a.address from ethereum.account a where a.address = account_getall.address;
+-- $$;
+--
+-- alter function ethereum.account_getall(char) owner to markr;
+--
+-- create or replace procedure ethereum.account_insert(IN name character varying, IN address character)
+--     language sql
+-- as
+-- $$
+--     INSERT INTO ethereum.account (name, address)
+--     VALUES (account_insert.name, account_insert.address);
+-- $$;
+--
+-- alter procedure ethereum.account_insert(varchar, char) owner to markr;
+--
+--
+-- create or replace function ethereum.get_meaning_of_life_universe_and_everything()
+--   returns int
+--   language sql
+-- AS
+-- $$
+-- SELECT 42;
+-- $$;
+--
+-- alter procedure ethereum.get_meaning_of_life_universe_and_everything() owner to markr;
